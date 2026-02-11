639a3e592c488a5e536b8873de847b14005c9d33
Idonotus <102253332+Idonotus@users.noreply.github.com>
2026-02-11 21:33:07 +0000

    chore: combine interaction and actions

--- src/engine/actions.rs
+++ src/engine/actions.rs
@@ -105,3 +105,17 @@ impl Response {
 trait Action {
 
 }
+
+enum Display {
+	ShowHand(u8),
+	ObfuscateHand(u8),
+
+}
+
+enum Prompt {
+	PickColor,
+	PickCardsFromHand(u8, u8),
+	PickNumeral,
+	PickPlayer,
+	Approval,
+}
